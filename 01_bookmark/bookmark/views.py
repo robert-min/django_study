c5c1ece2a47da2ef67eefa084dfeedbf2304118e
robertmin <robertmin522@dataslab.co.kr>
2022-07-15 11:38:16 +0900

    02 bookmark study - 1 (Finished)

--- 01_bookmark/bookmark/views.py
+++ 01_bookmark/bookmark/views.py
@@ -7,7 +7,9 @@ from django.shortcuts import render
 # 클래스형 뷰, 함수형 뷰
 # 웹 페이지 접속 -> 페이지 본다.(URL 입력 -> 웹서버가 뷰를 찾아서 동작 -> 응답)
 from django.views.generic.list import ListView
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic.detail import DetailView
+from django.urls import reverse_lazy
 from .models import Bookmark
 
 class BookmarkListView(ListView):
@@ -16,5 +18,17 @@ class BookmarkListView(ListView):
 class BookmarkCreateView(CreateView):
     model = Bookmark
     fields = ['site_name', 'url']
-
+    success_url = reverse_lazy('list')
     template_name_suffix = "_create"
+
+class BookmarkDetailView(DetailView):
+    model = Bookmark
+
+class BookmarkUpdateView(UpdateView):
+    model = Bookmark
+    fields = ['site_name', 'url']
+    template_name_suffix = "_update"
+
+class BookmarkDeleteView(DeleteView):
+    model = Bookmark
+    success_url = reverse_lazy('list')
