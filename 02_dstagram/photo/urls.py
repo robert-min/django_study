a006f5cdda147c5598d9a5b7fdf32efb84681d6a
robertmin <robertmin522@gmail.com>
2022-07-25 17:15:56 +0900

    photo delete finish

--- 02_dstagram/photo/urls.py
+++ 02_dstagram/photo/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 
 from .views import *
-
+# 2차 URL 파일
 app_name = "photo"
 
 urlpatterns = [
+    # class형은 as_view, 함수형은 바로 사용
     path('', photo_list, name="photo_list"),
+    path('upload/', PhotoUploadView.as_view(), name="photo_upload"),
+    path('delete/<int:pk>/', PhotoDeleteView.as_view(), name="photo_delete"),
 ]
--- 02_dstagram/photo/views.py
+++ 02_dstagram/photo/views.py
@@ -3,8 +3,33 @@ from django.shortcuts import render
 # Create your views here.
 from .models import Photo
 
+
+
 def photo_list(request):
     # 보여줄 데이터(사진)
     photos = Photo.objects.all()
-    # "object list"가 관례적 이름 -> "photos" 바꿀 수 있음.
+    # "object_list"가 관례적 이름 -> "photos" 바꿀 수 있음.
     return render(request, 'photo/list.html', {"photos": photos})
+
+from django.views.generic.edit import CreateView, DeleteView, UpdateView
+from django.shortcuts import redirect
+
+class PhotoUploadView(CreateView):
+    model = Photo
+    fields = ["photo", "text"] # 작성자(author), 작성시간(created)
+    template_name = "photo/upload.html"
+
+    def form_valid(self, form):
+        # author은 필수 필드
+        form.instance.author_id = self.request.user.id
+
+        if form.is_valid(): # 데이터가 올바르면 처리
+            form.instance.save()
+            return redirect("/")
+        else:
+            return self.render_to_response({"form": form})
+
+class PhotoDeleteView(DeleteView):
+    model = Photo
+    success_url = "/"
+    template_name = "photo/delete.html"
