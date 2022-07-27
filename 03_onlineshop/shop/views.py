0ea9f38c26cd833993c1b07f1cbedd187d7d00c1
robertmin <robertmin522@gmail.com>
2022-07-27 16:28:42 +0900

    static media s3 setting finish

--- 03_onlineshop/shop/views.py
+++ 03_onlineshop/shop/views.py
@@ -19,5 +19,5 @@ def product_in_category(request, category_slug=None):
 
 
 def product_detail(request, id, product_slug=None):
-    product = get_object_or_404(Product, slug=product_slug)
+    product = get_object_or_404(Product, id=id, slug=product_slug)
     return render(request, "shop/detail.html", {"product": product})
